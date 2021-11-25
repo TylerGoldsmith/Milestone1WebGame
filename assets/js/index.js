558b86d5a7afb325c42eb626bfcbcc8ed9169300
Tyler Goldsmith <tyler.r.goldsmith@gmail.com>
2021-11-24 19:31:45 -0500

    Fixed css and updated js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,41 +1,52 @@
-
 let wins = 0
 let loss = 0
 let draw = 0
+const userChoice = document.getElementById(".userChoice")
+const rockChoice = document.getElementById("#rockChoice")
+const paperChoice = document.getElementById("#paperChoice")
+const scissorChoice = document.getElementById("#scissorChoice")
+const userButton = document.getElementById(".userChoice button")
 
-let opponentsChoice = math.random();
-if (opponentsChoice < .34) {
-    opponent = "rockChoice";
-}
-else if (opponentsChoice > .67) {
-    opponentsChoice = "paperChoice";
-}
-else {
-    opponentsChoice = "scissorChoice";
-}
+function game() {
+    userButton.forEach(btn => {
+        
+        let opponentsChoice = Math.random();
+        if (opponentsChoice < .34) {
+            opponent = rockChoice;
+        }
+        else if (opponentsChoice > .67) {
+            opponentsChoice = paperChoice;
+        }
+        else {
+            opponentsChoice = scissorChoice;
+        }
 
-let battleOutcome = function (userChoice, opponentsChoice) {
-    if (userChoice === opponentsChoice)
-        return draw;
-}
-if (userChoice === "rockChoice") {
-    if (opponentsChoice === "scissorChoice") {
-        wins++;
-    } else {
-        return loss
+        let battleOutcome = function (userChoice, opponentsChoice) {
+            if (userChoice === opponentsChoice)
+                return draw;
+        }
+
+        if (userChoice === rockChoice) {
+            if (opponentsChoice === scissorChoice) {
+                return wins;
+            } else {
+                return loss;
+            }
+        }
+        if (userChoice === paperChoice) {
+            if (opponentsChoice === rockChoice) {
+                return wins;
+            } else {
+                return loss;
+            }
+        }
+        if (userChoice === scissorChoice) {
+            if (opponentsChoice === paperChoice) {
+                return wins;
+            } else {
+                return loss;
+            }
+        };
     }
-}
-if (userChoice === "paperChoice") {
-    if (opponentsChoice === "rockChoice") {
-        wins++;
-    } else {
-        return loss
-    }
-}
-if (userChoice === "scissorChoice") {
-    if (opponentsChoice === "paperChoice") {
-        wins++;
-    } else {
-        return loss
-    }
-};
+})
+game()
